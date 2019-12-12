66d0221b115d4d7d37991b9cde74742c4a323ca6
kapogon <kpogonowski@o2.pl>
2019-12-12 23:41:05 +0100

    Dodano kilka funkcji

--- sandbox/src/main/java/pl/qualitycourses/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/qualitycourses/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package pl.qualitycourses.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        hello("world");
+        hello("user");
+        hello("Leon");
+
+        double l = 5;
+        System.out.println("Pole kwadratu o boku " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 5;
+        System.out.println("Pole prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 }
