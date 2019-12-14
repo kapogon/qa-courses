33675a0a6084352e001e6d9b688940067a7c66bf
kapogon <kpogonowski@o2.pl>
2019-12-14 19:38:47 +0100

    Dodane klasy Square i Rectangle

--- sandbox/src/main/java/pl/qualitycourses/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/qualitycourses/sandbox/MyFirstProgram.java
@@ -7,23 +7,14 @@ public class MyFirstProgram {
         hello("user");
         hello("Leon");
 
-        double l = 5;
-        System.out.println("Pole kwadratu o boku " + l + " = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Pole kwadratu o boku " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 5;
-        System.out.println("Pole prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(4, 5);
+        System.out.println("Pole prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
-    }
-
-    public static double area(double len){
-        return len * len;
-    }
-
-    public static double area(double a, double b){
-        return a * b;
     }
 }
