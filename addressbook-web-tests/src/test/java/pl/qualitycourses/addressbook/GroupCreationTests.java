53b392bd17402131520b68f40b94da1835286231
kapogon <kpogonowski@o2.pl>
2019-12-22 11:40:18 +0100

    Dodany test usuwania grup i klasa bazowa

--- addressbook-web-tests/src/test/java/pl/qualitycourses/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/qualitycourses/addressbook/GroupCreationTests.java
@@ -1,78 +1,16 @@
 package pl.qualitycourses.addressbook;
 
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
-public class GroupCreationTests {
-  private WebDriver driver;
+public class GroupCreationTests extends TestBase {
 
-  @BeforeMethod
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/group.php");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
-    goToGroupPage();
-    initGroupCreation();
-    fillGroupForm(new GroupData("test1", "test2", "test3"));
-    createGroupCreation();
-    returnToGroupPage();
-  }
-
-  private void returnToGroupPage() {
-    driver.findElement(By.linkText("group page")).click();
-  }
-
-  private void createGroupCreation() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation() {
-    driver.findElement(By.name("new")).click();
-  }
-
-  private void goToGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
-  }
-
-  @AfterMethod
-  public void tearDown() throws Exception {
-    driver.quit();
-
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    @Test
+    public void testGroupCreation() throws Exception {
+        goToGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("test1", "test2", "test3"));
+        createGroupCreation();
+        returnToGroupPage();
     }
-  }
 
 }
---
+++ addressbook-web-tests/src/test/java/pl/qualitycourses/addressbook/TestBase.java
@@ -0,0 +1,80 @@
+package pl.qualitycourses.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+      driver = new FirefoxDriver();
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      driver.get("http://localhost/addressbook/group.php");
+      login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      driver.findElement(By.name("user")).click();
+      driver.findElement(By.name("user")).clear();
+      driver.findElement(By.name("user")).sendKeys(username);
+      driver.findElement(By.name("pass")).clear();
+      driver.findElement(By.name("pass")).sendKeys(password);
+      driver.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    protected void returnToGroupPage() {
+      driver.findElement(By.linkText("group page")).click();
+    }
+
+    protected void createGroupCreation() {
+      driver.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm(GroupData groupData) {
+      driver.findElement(By.name("group_name")).click();
+      driver.findElement(By.name("group_name")).clear();
+      driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+      driver.findElement(By.name("group_header")).clear();
+      driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+      driver.findElement(By.name("group_footer")).clear();
+      driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    protected void initGroupCreation() {
+      driver.findElement(By.name("new")).click();
+    }
+
+    protected void goToGroupPage() {
+      driver.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+      driver.quit();
+
+    }
+
+    public boolean isAlertPresent() {
+      try {
+        driver.switchTo().alert();
+        return true;
+      } catch (NoAlertPresentException e) {
+        return false;
+      }
+    }
+
+    protected void deleteSelectedGroups() {
+        driver.findElement(By.xpath("(//input[@name='delete'])[2]")).click();
+    }
+
+    protected void selectGroup() {
+        driver.findElement(By.name("selected[]")).click();
+    }
+}
