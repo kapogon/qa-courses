9c513ef17cd53f0b3d2b662649297fe1eb4587c7
kapogon <kpogonowski@o2.pl>
2019-12-27 18:36:12 +0100

    Dodany test dla modyfikacji grup

---
+++ addressbook-web-tests/src/test/java/pl/qualitycourses/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package pl.qualitycourses.addressbook.tests;
+
+import org.testng.annotations.Test;
+import pl.qualitycourses.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
