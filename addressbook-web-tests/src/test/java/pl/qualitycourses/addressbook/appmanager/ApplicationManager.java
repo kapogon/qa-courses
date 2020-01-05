c812c09d6d7f71b87cce6cc1a5a1ac81bbc0cf02
kapogon <kpogonowski@o2.pl>
2020-01-05 17:54:25 +0100

    Dodana możliwość zmiany przeglądarki dla testów

--- addressbook-web-tests/src/test/java/pl/qualitycourses/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/qualitycourses/addressbook/appmanager/ApplicationManager.java
@@ -3,20 +3,35 @@ package pl.qualitycourses.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     WebDriver driver;
+    private String browser;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        driver = new FirefoxDriver();
+        if(browser == BrowserType.FIREFOX) {
+            driver = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME){
+            driver = new ChromeDriver();
+        } else if (browser == BrowserType.IE){
+            driver = new InternetExplorerDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://localhost/addressbook/group.php");
         groupHelper = new GroupHelper(driver);
